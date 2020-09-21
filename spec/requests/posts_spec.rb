74f4cde6c31bdb9bb6cc93647721e79e6ebbafc6
Magdiel Marquez <magdielikari@gmail.com>
2020-09-20 23:12:13 -0500

    Add serialize and filter test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -8,7 +8,23 @@ RSpec.describe "post endpoint", type: :request do
       it "should return OK" do
         payload = JSON.parse(response.body)
         expect(payload).to be_empty
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
+      end
+    end 
+
+    describe "Search data" do    
+      let!(:hola_mundo) { create(:post, title: "Hola Mundo") }
+      let!(:hola_rails) { create(:post, title: "Hola Rails") }
+      let!(:curso_ruby) { create(:post, title: "Curso Ruby") }
+      it "should filter posts by title" do
+        get "/posts?search=Hola"
+        payload = JSON.parse(response.body)
+        expect(payload).to_not be_empty
+        expect(payload.size).to eq(2)
+        expect(payload.map{ |p| p["id"] }.sort).to eq([hola_mundo.id, hola_rails.id].sort)
+        expect(payload).to_not be_empty
+        expect(payload).to_not be_empty
+        expect(response).to have_http_status(:ok)
       end
     end 
 
@@ -19,7 +35,7 @@ RSpec.describe "post endpoint", type: :request do
         payload = JSON.parse(response.body)
         expect(payload).to_not be_empty
         expect(payload.size).to eq(posts.size)
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
   end
@@ -31,8 +47,90 @@ RSpec.describe "post endpoint", type: :request do
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
       expect(payload["id"]).to eq(post.id)
-      expect(response).to have_http_status(200)
+      expect(payload["title"]).to eq(post.title)
+      expect(payload["content"]).to eq(post.content)
+      expect(payload["published"]).to eq(post.published)
+      expect(payload["author"]["name"]).to eq(post.user.name)
+      expect(payload["author"]["email"]).to eq(post.user.email)
+      expect(payload["author"]["id"]).to eq(post.user.id)
+      expect(response).to have_http_status(:ok)
     end
   end
   
+  describe "POST /posts" do
+    let!(:user) { create(:user) }
+    it "should create a post" do
+      req_payload = {
+        post: {
+          title: "titulo",
+          content: "content",
+          published: false,
+          user_id: user.id
+        }
+      }
+      # POST HTTP
+      post "/posts", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload["id"]).to_not be_nil
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe "POST /posts" do
+    let!(:user) { create(:user) }
+    it "should return error message on invalid post" do
+      req_payload = {
+        post: {
+          content: "content",
+          published: false,
+          user_id: user.id
+        }
+      }
+      # POST HTTP
+      post "/posts", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload["error"]).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe "PUT /posts/{id}" do
+    let!(:post) { create(:post) }
+    it "should update a post" do
+      req_payload = {
+        post: {
+          title: "titulo",
+          content: "content",
+          published: true,
+        }
+      }
+      # POST HTTP
+      put "/posts/#{post.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload["id"]).to eq(post.id)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "PUT /posts/{id}" do
+    let!(:post) { create(:post) }
+    it "should return error message on invalid post" do
+      req_payload = {
+        post: {
+          title: nil,
+          content: nil,
+          published: true,
+        }
+      }
+      # POST HTTP
+      put "/posts/#{post.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload["error"]).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end

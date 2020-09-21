615f1a71ca21467605dd3bda9c756bfb7ab6138f
Magdiel Marquez <magdielikari@gmail.com>
2020-09-21 14:59:49 -0500

    Add cache feature, Todo, validate

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,8 @@
 class PostsSearchService
   def self.search(curr_post, query)
-    curr_post.where("title like '%#{query}%'")
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in:1.hours) do
+      curr_post.where("title like '%#{query}%'").map(&:id)      
+    end
+    curr_post.where(id: posts_ids)
   end
 end

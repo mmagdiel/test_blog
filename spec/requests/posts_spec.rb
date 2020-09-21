736b51984f77a4b8e5695ee93114cfab31244728
Magdiel Marquez <magdielikari@gmail.com>
2020-09-20 20:38:49 -0500

    Add controllers for get posts endpoints

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "post endpoint", type: :request do
 
-  describe "GET /post" do
+  describe "GET /posts" do
     describe "without data in the BD" do    
-      before { get '/post' }
+      before { get '/posts' }
       it "should return OK" do
         payload = JSON.parse(response.body)
         expect(payload).to be_empty
@@ -13,8 +13,8 @@ RSpec.describe "post endpoint", type: :request do
     end 
 
     describe "with data in the BD" do
-      before { get '/post' }
-      let(:posts) { crate_list(:post, 10, published: true) } # create_list: factory bot, let: rspec
+      let!(:posts) { create_list(:post, 10, published: true) } # create_list: factory bot, let: rspec
+      before { get '/posts' }
       it "should return status code 200" do
         payload = JSON.parse(response.body)
         expect(payload).to_not be_empty
@@ -23,14 +23,16 @@ RSpec.describe "post endpoint", type: :request do
       end
     end
   end
-  describe "GET /post/{id}" do
-    let(:post) { create(:post) }
+
+  describe "GET /posts/{id}" do
+    let!(:post) { create(:post) }
     it "should return a post" do
-      get "/post/#{post.id}"
+      get "/posts/#{post.id}"
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
-      expect(payload[]).to eq(post.id)
+      expect(payload["id"]).to eq(post.id)
       expect(response).to have_http_status(200)
     end
   end
+  
 end
